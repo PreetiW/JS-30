0a08ffab2d35e18fb8362dac8c4c921fae020547
Preeti Wadhwani <Preeti Wadhwani>
2017-01-19 16:00:01 +0530

    Day 5 and 6 of Javascript 30

--- 04 - Array Cardio Day 1/Node js/arrayExample.js
+++ 04 - Array Cardio Day 1/Node js/arrayExample.js
@@ -12,11 +12,29 @@
       { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
       { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
       { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+       { first: 'bada', last: 'Lovelace', year: 1815, passed: 1852 },
       { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
       { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
       { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
       { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
     ];
+
+    const byLastName = inventors.sort(function(a, b){
+
+     if(a.last === b.last)
+      {
+          console.log("both are equal");
+      
+            return a.first > b.first ? 1 : -1;
+        
+      }
+      else
+      {
+        console.log("both are not equal");
+        return a.last > b.last ? 1 : -1;
+      
+      }
+    });
 
     const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
